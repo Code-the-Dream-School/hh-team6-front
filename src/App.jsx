9894291d551f1b837934591ae94e3696cd94b8f6
brandon91warren <brandon91warren@github.com>
2024-11-14 20:49:04 -0800

    [RB-10] Add fonts and minor changes to vite and app.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { getAllData } from './util/index';
-
-const URL = 'http://localhost:8000/api/v1/';
-
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    (async () => {
-      const myData = await getAllData(URL);
-      setMessage(myData.data);
-    })();
-
-    return () => {
-      console.log('unmounting');
-    };
-  }, []);
-
   return <></>;
 }
 
