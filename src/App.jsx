c25091095645e80a115b4f9ccd33c41a5761097d
brandon91warren <brandon91warren@github.com>
2024-11-17 21:12:47 -0800

    [RB-14] create 404 not found page and example home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { getAllData } from './util/index';
 
 const URL = 'http://localhost:8000/api/v1/';
@@ -17,10 +18,30 @@ function App() {
     };
   }, []);
 
+  const HomePage = () => (
+    <div>
+      <h1>ReBooks</h1>
+      <p>{message}</p>
+    </div>
+  );
+
+  const NotFoundPage = () => (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <a href="/">Go back to Home</a>
+    </div>
+  );
+
   return (
-    <>
-      <h1>{message}</h1>
-    </>
+    <Router>
+      <Routes>
+        {/* Define routes */}
+        <Route path="/" element={<HomePage />} />
+        {/* Catch-all for 404 */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
 
