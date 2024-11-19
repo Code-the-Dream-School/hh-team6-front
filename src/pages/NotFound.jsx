346653661140e96ee7d0e6604f4ca45205120c53
brandon91warren <brandon91warren@github.com>
2024-11-18 17:25:10 -0800

    [RB-14] 404 Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import notFoundImage from '../assets/images/404.jpg';
 
 const NotFound = () => {
-  return <></>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <img
+        src={notFoundImage}
+        alt="Page not found"
+        className="w-full max-w-4xl" 
+      />
+      <a
+        href="/"
+        className="text-black underline"
+      >
+        Back to Home Page
+      </a>
+    </div>
+  );
 };
 
 export default NotFound;
