c25c578ee99e6e2e05d28a8775d6d8d54498c673
Evgenii Rychkov <evgeniirychkov@Evgeniis-MacBook-Pro.local>
2024-11-19 23:57:53 -0800

    [RB-31] Create Sign Up page

---
+++ src/components/Form/Input.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = ({ id, name, type, value, onChange, children, error }) => (
+  <div className="mb-3 flex w-full flex-col gap-1">
+    <label htmlFor={id}>{children}</label>
+    <input
+      id={id}
+      name={name}
+      type={type}
+      value={value}
+      onChange={onChange}
+      className={'w-full rounded border-2 border-gray p-2'}
+    />
+    {error && <p className="text-red">{error}</p>}
+  </div>
+);
+
+Input.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  onChange: PropTypes.func.isRequired,
+  children: PropTypes.node,
+  error: PropTypes.string,
+};
+
+export default Input;
