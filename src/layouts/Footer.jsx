f7beb68a590c434c16c3841497eeeb0f27831d3a
brandon91warren <brandon91warren@github.com>
2024-11-21 09:39:52 -0800

    [RB-13] Add Footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import footerImage from '../assets/images/footer.jpg'; //
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <img 
-                src={footerImage} 
-                alt="Footer" 
-            />
-        </footer>
-    );
+  return (
+      <footer style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <img 
+              src={footerImage} 
+              alt="Footer" 
+              style={{ width: '100%', height: 'auto' }} 
+          />
+      </footer>
+  );
 };
 
 export default Footer;
-
