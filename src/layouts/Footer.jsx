89b8dd3e515b0f1cf7dcaedc3e51930ef60f1380
brandon91warren <brandon91warren@github.com>
2024-11-21 15:22:45 -0800

    [RB-13] Add footer remove footerBg

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,11 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="bg-footerBg flex justify-center py-6">
-  <p className="text-sm text-black">
-    Created and developed by Team #6 for the CTD Practicum
-  </p>
-</footer>
-
+    <footer className="bg-lightBlue flex justify-center py-6">
+      <p className="text-sm text-black">
+        Created and developed by Team #6 for the CTD Practicum
+      </p>
+    </footer>
   );
 };
 
