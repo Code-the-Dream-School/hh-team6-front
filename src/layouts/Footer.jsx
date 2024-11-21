fbcf8b60697626444bccfdf1cbd3ef34272793e6
brandon91warren <brandon91warren@github.com>
2024-11-21 12:29:23 -0800

    [RB-13] check linter and npm run format for changes to footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-black py-6">
+    <footer className="bg-gray-800 py-6 text-black">
       <div className="container mx-auto text-center">
         <p className="text-sm">
           Created and developed by Team #6 for the CTD Practicum
