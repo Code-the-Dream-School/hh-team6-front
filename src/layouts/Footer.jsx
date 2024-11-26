754495e56fde61aaacd353597cc6bf157620fa37
Evgenii Rychkov <evgeniirychkov@Evgeniis-MacBook-Pro.local>
2024-11-26 15:46:32 -0800

    add text-center to Footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="mt-5 flex justify-center bg-lightBlue py-6">
       <Link to="/about">
-        <p className="text-sm text-black">
+        <p className="text-center text-sm text-black">
           Created and developed by Team #6 for the CTD Practicum
         </p>
       </Link>
