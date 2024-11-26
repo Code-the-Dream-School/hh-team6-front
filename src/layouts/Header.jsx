74f0cfe43eb1c3a5770c386b2836c38ed87920fd
Evgenii Rychkov <evgeniirychkov@Evgeniis-MacBook-Pro.local>
2024-11-26 11:26:45 -0800

    Change Div to Link

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -2,25 +2,19 @@ import React from 'react';
 import logoImage from '../assets/images/logo.png';
 import Search from '../components/Search';
 import NavBar from '../components/NavBar';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const navigate = useNavigate();
-
-  const handleHomeNavigation = () => {
-    navigate('/');
-  };
-
   return (
     <div className="p-5">
       <div className="flex items-center justify-between">
-        <div className="flex items-center" onClick={handleHomeNavigation}>
+        <Link to="/" className="flex items-center">
           <img
             className="h-[26px] w-[33px] sm:h-[45px] sm:w-[60px]"
             src={logoImage}
           />
           <h1 className="font-headings sm:text-2xl sm:font-bold">Re:Books</h1>
-        </div>
+        </Link>
         <div className="hidden max-w-[550px] px-5 sm:block sm:flex-auto">
           <Search id="search" />
         </div>
