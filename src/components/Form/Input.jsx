e720a15c9db71418786b63fff4c8db253cb10d82
EvgeniiRychkov <98305332+EvgeniiRychkov@users.noreply.github.com>
2024-11-28 12:45:46 -0800

    [RB-11] Header
    [RB-11] Create Header

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Input as HeadlessInput } from '@headlessui/react';
 
 const Input = ({ id, name, type, value, onChange, children, error }) => (
   <div className="mb-3 flex w-full flex-col gap-1">
     <label htmlFor={id}>{children}</label>
-    <input
+    <HeadlessInput
       id={id}
       name={name}
       type={type}
