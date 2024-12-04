6e1278b9d8e69b5c2089a6d9c397fc39718dddc7
brandon91warren <brandon91warren@github.com>
2024-12-04 15:38:10 -0800

    [RB-21] Profile Page (Account Folder)

--- src/pages/account/Profile.jsx
+++ src/pages/account/Profile.jsx
@@ -6,35 +6,35 @@ import userIcon from '../../assets/images/user.png';
 
 const Profile = () => {
   return (
-    <div className="text-left font-sans">
+    <div className="font-sans">
       <h1 className="mb-6 font-headings text-2xl font-bold">
         Personal Profile
       </h1>
-      <div className="mb-4 flex items-center">
+      <div className="mb-6 flex items-center">
         <img
           src={userIcon}
           alt="User Icon"
-          className="text-gray-500 mr-6 h-6 w-6"
+          className="h-6 w-6"
         />
-        <p className="font-body text-2xl">John Doe</p>
+        <p className="ml-[35px] text-2xl">John Doe</p>
       </div>
-      <div className="mb-4 flex items-center">
+      <div className="mb-6 flex items-center">
         <img
           src={envelopeIcon}
           alt="Envelope Icon"
-          className="text-gray-500 mr-6 h-6 w-6"
+          className="h-6 w-6"
         />
-        <p className="font-body text-2xl">john-doe@mail.com</p>
+        <p className="ml-[35px] text-2xl">john-doe@mail.com</p>
       </div>
       <div className="mb-6 flex items-center">
         <img
           src={locationIcon}
           alt="Location Icon"
-          className="text-gray-500 mr-6 h-6 w-6"
+          className="h-6 w-6"
         />
-        <p className="font-body text-2xl">San Diego, CA</p>
+        <p className="ml-[35px] text-2xl">San Diego, CA</p>
       </div>
-      <button className="w-full rounded bg-orange-500 px-4 py-2 text-center font-semibold text-white hover:bg-orange-600 sm:w-80">
+      <button className="w-full rounded bg-yellow px-4 py-2 text-center font-semibold text-white hover:bg-yellow sm:w-80">
         Edit Profile
       </button>
     </div>
