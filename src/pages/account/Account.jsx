a342ec0ca99a8ccf15d0916382e371519a905183
brandon91warren <brandon91warren@github.com>
2024-12-04 17:34:19 -0800

    [RB-22] Profile Edit Page

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,5 +1,33 @@
+import Profile from './Profile';
+import LeftMenu from '../../components/account/LeftMenu';
+import { useAccount } from '../../context/AccountProvider';
+
 const Account = () => {
-  return <></>;
+  const { accountPage } = useAccount();
+
+  const accountContent = () => {
+    switch (accountPage) {
+      case 'addBook':
+        return <div>addBook</div>;
+      case 'myBooks':
+        return <div>myBooks</div>;
+      case 'orderHistory':
+        return <div>orderHistory</div>;
+      case 'savedBooks':
+        return <div>savedBooks</div>;
+      case 'messages':
+        return <div>messages</div>;
+      case 'profile':
+        return <Profile />;
+    }
+  };
+
+  return (
+    <div className="flex flex-grow">
+      <LeftMenu />
+      <div className="flex-1 p-6">{accountContent()}</div>
+    </div>
+  );
 };
 
 export default Account;
--- src/pages/account/Profile.jsx
+++ src/pages/account/Profile.jsx
@@ -1,37 +1,28 @@
 import React from 'react';
-import userIcon from '../../assets/images/user.png';
-import locationIcon from '../../assets/images/location.png';
+
 import envelopeIcon from '../../assets/images/envelope.png';
+import locationIcon from '../../assets/images/location.png';
+import userIcon from '../../assets/images/user.png';
 
 const Profile = () => {
   return (
-    <div className="mx-auto max-w-sm text-left font-sans">
-      <h1 className="mb-6 text-xl font-bold">Personal Profile</h1>
-      <div className="mb-4 flex items-center">
-        <img
-          src={userIcon}
-          alt="User Icon"
-          className="text-gray-500 mr-3 h-6 w-6"
-        />
-        <span className="text-base">John Doe</span>
-      </div>
-      <div className="mb-4 flex items-center">
-        <img
-          src={envelopeIcon}
-          alt="Envelope Icon"
-          className="text-gray-500 mr-3 h-6 w-6"
-        />
-        <span className="text-base">john-doe@mail.com</span>
+    <div className="font-sans">
+      <h1 className="mb-6 font-headings text-2xl font-bold">
+        Personal Profile
+      </h1>
+      <div className="mb-6 flex items-center">
+        <img src={userIcon} alt="User Icon" className="h-6 w-6" />
+        <p className="ml-[35px] text-2xl">John Doe</p>
       </div>
       <div className="mb-6 flex items-center">
-        <img
-          src={locationIcon}
-          alt="Location Icon"
-          className="text-gray-500 mr-3 h-6 w-6"
-        />
-        <span className="text-base">San Diego, CA</span>
+        <img src={envelopeIcon} alt="Envelope Icon" className="h-6 w-6" />
+        <p className="ml-[35px] text-2xl">john-doe@mail.com</p>
       </div>
-      <button className="w-full rounded bg-orange-500 px-4 py-2 text-center font-semibold text-white hover:bg-orange-600">
+      <div className="mb-6 flex items-center">
+        <img src={locationIcon} alt="Location Icon" className="h-6 w-6" />
+        <p className="ml-[35px] text-2xl">San Diego, CA</p>
+      </div>
+      <button className="w-full rounded bg-yellow px-4 py-2 text-center font-semibold text-white hover:bg-yellow sm:w-80">
         Edit Profile
       </button>
     </div>
