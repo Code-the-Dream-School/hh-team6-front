c74f900f215ed7ee7095663c4392815249b80a14
Liuba <liubabarusch@gmail.com>
2024-12-08 16:01:12 -0600

    [RB-15] Fix alignment

--- src/components/Books/BookCard.jsx
+++ src/components/Books/BookCard.jsx
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 
 const BookCard = ({ img, title, author }) => {
   return (
-    <div className="flex w-52 flex-col rounded bg-lightBlue px-6 py-5">
-      <img className="h-auto w-32 self-center" src={img} alt="Book" />
-      <div className="pt-4">
-        <h2 className="mb-2 font-bold">{title}</h2>
-        <p className="text-gray-700"> by {author}</p>
+    <div className="flex w-52 max-w-52 flex-col items-center rounded bg-lightBlue p-4 shadow-md lg:w-40 xl:w-52">
+      <img className="h-auto w-32" src={img} alt={`Cover of ${title}`} />
+      <div className="pt-4 text-center">
+        <h2 className="mb-2 text-lg font-bold">{title}</h2>
+        <p className="text-gray-700 text-sm">by {author}</p>
       </div>
     </div>
   );
--- src/components/Books/BooksList.jsx
+++ src/components/Books/BooksList.jsx
@@ -4,20 +4,29 @@ import BookCard from './BookCard';
 
 const BooksList = ({ list }) => {
   return (
-    <div className="flex flex-col flex-wrap gap-4 md:flex-row md:gap-16">
-      {list.map((item, index) => (
-        <BookCard
-          key={index}
-          img={item.image}
-          title={item.title}
-          author={item.author}
-        />
-      ))}
+    <div className="container mx-auto">
+      <div className="grid justify-items-center gap-4 sm:grid-cols-2 sm:gap-6 md:grid-cols-3 md:gap-8 lg:grid-cols-5">
+        {list.map((item, index) => (
+          <BookCard
+            key={index}
+            img={item.image}
+            title={item.title}
+            author={item.author}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
 BooksList.propTypes = {
-  list: PropTypes.array.isRequired,
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      image: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
+
 export default BooksList;
