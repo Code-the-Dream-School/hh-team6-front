26f22a72a2aecdea167e96a4eb512d545ebdd226
Liuba <liubabarusch@gmail.com>
2024-12-25 15:54:45 -0800

    [RB-24] Minor change

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -24,7 +24,7 @@ const SignIn = () => {
             id="email"
             name="email"
             type="email"
-            value={(form.email = 'test@test.com')}
+            value={form.email}
             error={error.email}
             onChange={handleChange}
           >
@@ -35,7 +35,7 @@ const SignIn = () => {
             id="password"
             name="password"
             type="password"
-            value={(form.password = 'testtest')}
+            value={form.password}
             error={error.password}
             onChange={handleChange}
           >
