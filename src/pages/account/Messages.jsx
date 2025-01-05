4fc0b0a4b64d7aed16408869ca0f8fa65988e1e2
Valya <vrudnitskaya@gmail.com>
2025-01-04 22:58:00 -0600

    [RB-99]add sockets to messages

--- src/pages/account/Messages.jsx
+++ src/pages/account/Messages.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback, useRef } from 'react';
 
 import { Button } from '@headlessui/react';
-
+import io from 'socket.io-client';
 import { getChats, getChatMessages } from '../../api/DBRequests';
 import SendMessage from '../../components/account/SendMessage';
 import { useAccount } from '../../context/AccountProvider';
@@ -18,6 +18,30 @@ const Messages = () => {
   const [error, setError] = useState('');
   const [errorMessages, setErrorMessages] = useState('');
   const lastMessageRef = useRef(null);
+  const socketRef = useRef();
+  
+  useEffect(() => {
+    socketRef.current = io('http://localhost:8000');
+
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (currentChatId) {
+      socketRef.current.emit('joinChat', currentChatId);
+
+      socketRef.current.on('newMessage', (message) => {
+        console.log('mes',message)
+        setMessages((prev) => [...prev, message]);
+      });
+
+      return () => {
+        socketRef.current.off('newMessage');
+      };
+    }
+  }, [currentChatId]);
 
   const fetchMessages = useCallback(
     async (id) => {
